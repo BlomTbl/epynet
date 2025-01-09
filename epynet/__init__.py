267dc2fe6ba6ddb3e2be0376ba0e90e5d27ad4eb
BlomTbl <ton.blom@vitens.nl>
2025-01-09 09:53:01 +0100

    minor change

--- epynet/__init__.py
+++ epynet/__init__.py
@@ -1,5 +1,8 @@
-from .network import Network
+from .network import Network,epynetvalues
 from .link import Link, Pipe, Pump, Valve
 from .node import Node, Junction, Reservoir, Tank
 from .objectcollection import ObjectCollection
-from .outbin import OutBinNode, OutBinLink, EpanetOutBin
+from .epaexport import epynetvalues
+from epynet import gis
+from .outbin import EpanetOutBin
+__version__ = '1.0.0'
